3fb240d7b3c68019834ed9e21ac62435ea410e05
Matthew Watkins <mwatkins@linuxfoundation.org>
2025-02-13 16:53:14 +0000

    Chore: Add libnsl
    Signed-off-by: Matthew Watkins <mwatkins@linuxfoundation.org>

--- sonarqube-cloud-build.sh
+++ sonarqube-cloud-build.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 sudo apt-get -y update
-sudo apt-get -y install libxml2-dev libsctp-dev libpcap-dev
+sudo apt-get -y install libxml2-dev libsctp-dev libpcap-dev libnsl
 cd build; make
