c15aa91e57e0aaaf7cc93fe878e8f4d52be63c21
Matthew Watkins <mwatkins@linuxfoundation.org>
2025-02-13 16:46:50 +0000

    Chore: Add sudo command to package installation
    Signed-off-by: Matthew Watkins <mwatkins@linuxfoundation.org>

--- sonarqube-cloud-build.sh
+++ sonarqube-cloud-build.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
-apt-get -y update
-apt-get -y install libxml2-dev libsctp-dev libpcap-dev
+sudo apt-get -y update
+sudo apt-get -y install libxml2-dev libsctp-dev libpcap-dev
 cd build; make
